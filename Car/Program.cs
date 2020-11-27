d06dee9f6bd244f61c2264ca90779d189e4df97a
Opilane TTHK <opilane@tthk.ee>
2020-11-27 09:06:22 +0200

    first commit

---
+++ Car/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace Car
+{
+    class Program
+    {
+
+        class Car
+        {
+            string name;
+            string model;
+            string registration;
+            string color;
+            int odoMeter;
+            int fuelTank;
+
+            public Car(string _name, string _model, string _registration, string _color, int _odoMeter, int _fuelTank)
+            {
+                name = _name;
+                model = _model;
+                color = _color;
+
+                if(registration.Length != 6)
+                {
+                    registration = "undefined";
+                }
+                else
+                {
+                    registration = _registration;
+                }
+
+
+                odoMeter = 0;
+                fuelTank = 60;
+            }
+
+            public string Registration
+            {
+                get { return registration; }
+            }
+
+            public string Name
+            {
+                get { return name; }
+            }
+
+            public string Model
+            {
+                get { return model; }
+            }
+
+            public string Color
+            {
+                get { return color; }
+            }
+
+            public int FuelTank
+            {
+                get { return fuelTank; }
+            }
+
+            public int OdoMeter
+            {
+                get { return odoMeter; }
+            }
+
+            public void DoALap()
+            {
+                Console.WriteLine("The car drives a lap.");
+                odoMeter += 100;
+                fuelTank -= 5;
+                Console.WriteLine($"Odometer: {odoMeter}; Fuel Tank: {fuelTank}");
+            }
+
+            public void CarStats()
+            {
+                Console.WriteLine($"Name: {name}\n Model: {model}\n Registration: {registration}\n Color: {color}\n Odometer: {odoMeter}\n Fuel Tank: {fuelTank}");
+            }
+            
+        }
+
+
+        static void Main(string[] args)
+        {
+            Car newCar = new Car("Toyota", "Supra", "123ABC", "Blue", 0, 60);
+
+            while(newCar.FuelTank != 0)
+            {
+                newCar.DoALap();
+            }
+
+            newCar.CarStats();
+            Console.WriteLine("The ride is over.");
+        }
+    }
+}
---
+++ OOPChallenges/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace OOPChallenges
+{
+    class Program
+    {
+
+        class Dog
+        {
+            string name;
+            int spots;
+            double happiness;
+
+            public Dog(string _name, int _spots)
+            {
+                name = _name;
+                spots = _spots;
+                happiness = 0;
+                Console.WriteLine($"The dog {name} has been created.");
+            }
+
+            public double Happiness
+            {
+                get { return happiness; }
+            }
+
+            public void Barks()
+            {
+                Console.WriteLine("Woof-woof");
+                happiness += 0.2;
+            }
+
+            public void Wiggle()
+            {
+                Console.WriteLine("Wiggle-wiggle");
+            }
+
+            public void Rename(string newName)
+            {
+                name = newName;
+            }
+
+            public void ShowDogData()
+            {
+                Console.WriteLine($"Name: {name}; Spots: {spots}; Happiness: {happiness};");
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Dog newDog = new Dog("Koer", 10);
+
+            Console.WriteLine($"The level of happiness: {newDog.Happiness}");
+
+            while (newDog.Happiness != 1)
+            {
+                newDog.Barks();
+            }
+
+            newDog.Wiggle();
+
+            Console.WriteLine("Name the dog: ");
+            string newDogName = Console.ReadLine();
+            newDog.Rename(newDogName);
+            newDog.ShowDogData();
+        }
+    }
+}
